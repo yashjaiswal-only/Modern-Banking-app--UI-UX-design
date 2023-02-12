676e43631a30889ff10155bd57b1efa9de67632a
Yash Jaiswal <yashjaiswal2121@gmail.com>
2023-02-13 01:34:53 +0530

    hoobank initialized

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,20 @@
+import {card} from '../assets'
+import styles,{layout} from '../style'
+import Button from './Button'
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal 
+      <br className='sm:block hidden'/>in few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eius beatae officiis repudiandae eum quod aut quo debitis dolorum quis, consequuntur adipisci nobis est incidunt expedita aperiam voluptatem esse deserunt!
+      </p>
+      <Button styles="mt-10"/>
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className='w-[100%] h-[100%]'/>
+    </div>
+  </section>
+)
+export default CardDeal
